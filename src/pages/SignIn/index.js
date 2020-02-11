9f467d5e73551609f524387824a7e300e579441b
Felipe Reis <ffrancis.reis@gmail.com>
2020-02-11 02:11:09 -0300

    Add useRef, focus

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Image } from 'react-native';
 import logo from '~/assets/logo.png';
 import Background from '~/components/Background';
@@ -12,6 +12,10 @@ import {
 } from './styles';
 
 export default function SignIn({ navigation }) {
+  const passwordRef = useRef();
+
+  function handleSubmit() {}
+
   return (
     <Background>
       <Container>
@@ -24,12 +28,19 @@ export default function SignIn({ navigation }) {
             autoCorrect={false}
             autoCapitalize="none"
             placeholder="Digite seu e-mail"
+            returnKeyType="next"
+            onSubmitEditing={() => {
+              passwordRef.current.focus();
+            }}
           />
 
           <FormInput
             icon="lock-outline"
             secureTextEntry
             placeholder="Sua senha secreta"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
           />
 
           <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
